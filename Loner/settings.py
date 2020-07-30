286f0808dac13a756aa5d05979280666607ff4e5
Aayushma pant <34135400+Ayushma99@users.noreply.github.com>
2020-07-30 23:38:24 +0545

    Update settings.py

--- Loner/settings.py
+++ Loner/settings.py
@@ -28,6 +28,11 @@ DEBUG = False
 ALLOWED_HOSTS = ['https://django-deploymentml.herokuapp.com/']
 CRISPY_TEMPLATE_PACK="bootstrap4"
 STATIC_URL="/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'Myapi/static')
+)
 
 # Application definition
 
