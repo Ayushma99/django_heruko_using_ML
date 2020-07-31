fbe6543c49cb6509622f5fae9ab11d7d664430a5
Aayushma pant <34135400+Ayushma99@users.noreply.github.com>
2020-07-31 18:42:19 +0545

    Update settings.py

--- Loner/settings.py
+++ Loner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#@r6pkpix@zg%52uge0zymieya5$5d+lx$a_og3_spu3)0+n0&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
